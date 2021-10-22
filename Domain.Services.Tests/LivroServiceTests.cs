7f7149355a90acf5bb0159dcb8c40cb96e24ebfd
Victor Fructuoso <victor@fructuoso.com.br>
2021-10-22 17:50:02 -0300

    tests: Testando LivroService.UpdateAsync

--- Domain.Services.Tests/LivroServiceTests.cs
+++ Domain.Services.Tests/LivroServiceTests.cs
@@ -11,11 +11,13 @@ namespace Domain.Services.Tests
     public class LivroServiceTests
     {
         /*
-        Task<TEntity> AddAsync(TEntity obj);
+        OK - Task<TEntity> AddAsync(TEntity obj);
+        Task<TEntity> UpdateAsync(TEntity obj);
+        
+
         Task<TEntity> DeleteAsync(TKey id);
         Task<TEntity> GetAsync(TKey id);
         Task<IEnumerable<TEntity>> GetAllAsync();
-        Task<TEntity> UpdateAsync(TEntity obj);
         */
 
         [Fact(DisplayName = "DADO um Livro válido QUANDO chamado o GenericServiceCrud.AddAsync ENTÃO chamar o IRepositoryCrud.AddAsync")]
@@ -33,6 +35,23 @@ namespace Domain.Services.Tests
 
             //Assert
             repositorio.Verify(o => o.AddAsync(livro), Times.Once());
+        }
+
+        [Fact(DisplayName = "DADO um Livro válido QUANDO chamado o GenericServiceCrud.UpdateAsync ENTÃO chamar o IRepositoryCrud.AddAsync")]
+        public async Task UpdateAsync()
+        {
+            //Arrange
+            var repositorio = new Mock<IRepositoryCrud<int, Livro>>();
+
+            var livro = new Livro() { Id = 1, Nome = "DOMAIN DRIVEN DESIGN", Autor = "EVANS" };
+
+            var livroService = new GenericServiceCrud<int, Livro>(repositorio.Object);
+
+            //Act
+            await livroService.UpdateAsync(livro);
+
+            //Assert
+            repositorio.Verify(o => o.UpdateAsync(livro), Times.Once());
         }
     }
 }
